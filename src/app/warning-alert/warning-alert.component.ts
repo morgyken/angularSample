79a088b03c641307f0f1f9bbf8ee48048e40b739
Morgyken <Morgyken@gmail.com>
2020-03-29 09:05:52 +0300

    First Commit

---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,11 @@
+
+import {Component} from '@angular/core';
+
+
+@Component({
+    template: `<p> This a warning template </p>`,
+    selector: 'app-warning-alert'
+}) 
+export class WarningAlertComponent {
+
+}
